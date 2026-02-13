df8a09125d25c2cad6071380e4dbd5e8c3974471
Bruno Vitte <bruno_vitte@hotmail.com>
2026-02-13 00:24:19 -0600

    Refactor formatting in premake5.lua for consistency and readability

--- Lore/vendor/GLAD/premake5.lua
+++ Lore/vendor/GLAD/premake5.lua
@@ -1,33 +1,32 @@
 project "GLAD"
-kind "StaticLib"
-language "C"
-staticruntime "on"
+	kind "StaticLib"
+	language "C"
+	staticruntime "on"
 
-targetdir("bin/" .. outputdir .. "/%{prj.name}")
-objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
 
-files
-{
+	files {
 	"include/glad/glad.h",
 	"include/KHR/khrplatform.h",
 	"src/glad.c",
-}
+	}
 
-includedirs {
-	"include"
-}
+	includedirs {
+		"include"
+	}
 
-filter "system:macosx"
-architecture 'ARM64'
+	filter "system:macosx"
+		architecture 'ARM64'
 
-filter "system:windows"
-architecture 'x86_64'
-systemversion "latest"
+	filter "system:windows"
+		architecture 'x64'
+		systemversion "latest"
 
-filter "configurations:Debug"
-runtime "Debug"
-symbols "on"
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
 
-filter "configurations:Release"
-runtime "Release"
-optimize "on"
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
